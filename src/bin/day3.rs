a56fadc1fdc4859d595a8ca72108d5cb3ba52df7
Daniel Boyle <boylede+github@gmail.com>
2021-12-13 22:00:59 -0500

    shorten day3

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,21 +1,28 @@
+// day 3
+// gamma: 3903
+// epsilon: 192
+// parta: 749376
+// oxygen: 3871
+// scrubber: 613
+// partb: 2372923
+
 const INPUT: &str = include_str!("../../input/day3.txt");
+
+const TEST_INPUT: &str = "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010";
 
 fn main() {
     println!("day 3");
-    //     let input = "00100
-    // 11110
-    // 10110
-    // 10111
-    // 10101
-    // 01111
-    // 00111
-    // 11100
-    // 10000
-    // 11001
-    // 00010
-    // 01010";
-    // 749376
-
     let bit_width = INPUT.lines().next().unwrap().len();
     {
         let gamma = INPUT
@@ -26,186 +33,79 @@ fn main() {
                     .map(|c| if c == '1' { (1, 0) } else { (0, 1) })
                     .collect()
             })
-            .reduce(|mut bits: Vec<(u32, u32)>, current: Vec<(u32, u32)>| {
-                bits.iter_mut()
-                    .zip(current.iter())
-                    .for_each(|((t, f), (nt, nf))| {
-                        *t += nt;
-                        *f += nf;
-                    });
-                bits
+            .reduce(|mut totals: Vec<(u32, u32)>, next: Vec<(u32, u32)>| {
+                totals.iter_mut().zip(next.iter()).for_each(
+                    |((trues, falses), (next_true, next_false))| {
+                        *trues += next_true;
+                        *falses += next_false;
+                    },
+                );
+                totals
             })
             .unwrap()
             .iter()
             .enumerate()
-            .fold(0, |a, (position, (t, f))| {
-                let set = if t > f { 1u32 } else { 0u32 };
-                a | set << position
+            .fold(0, |accumulator, (position, (trues, falses))| {
+                let bit = if trues > falses { 1u32 } else { 0u32 };
+                accumulator | bit << position
             });
-        let bit_mask = (1 << 12) - 1;
+        let bit_mask = (1 << bit_width) - 1;
         let epsilon = (!gamma) & bit_mask;
         let parta = gamma * epsilon;
         println!("parta: {}", parta);
-        // assert!(749376 == parta);
     }
 
     {
-        // let mut bit_accumulator: Vec<Vec<(u32, u32)>>  = input
-        //     .lines()
-
-        //     }).collect();
         let diagnostics: Vec<Vec<u32>> = INPUT
             .lines()
-            .map(|s| -> Vec<u32> {
-                s.chars()
-                    // .rev()
-                    .map(|c| if c == '1' { 1 } else { 0 })
-                    .collect()
-            })
+            .map(|s| -> Vec<u32> { s.chars().map(|c| if c == '1' { 1 } else { 0 }).collect() })
             .collect();
-        for bit in 0..12 {
-            println!("considering bit {}", bit);
-            let mut remove_list = Vec::new();
-            let bit_counts: (u32, u32) =
-                diagnostics
-                    .iter()
-                    .filter_map(|v| v.get(bit))
-                    .fold(
-                        (0, 0),
-                        |(t, f), i| {
-                            if *i == 1 {
-                                (t + 1, f)
-                            } else {
-                                (t, f + 1)
-                            }
-                        },
-                    );
-            println!("there are {} set and {} unset", bit_counts.0, bit_counts.1);
-            let most_common_bit = if bit_counts.0 >= bit_counts.1 { 1 } else { 0 };
-            println!("therefor the most common is {}", most_common_bit);
-            for (i, diagnostic) in diagnostics.iter().enumerate() {
-                if diagnostic[bit] != most_common_bit {
-                    remove_list.push(i);
-                    println!("will remove {}", i);
-                }
-            }
-
-            diagnostics = diagnostics
+        let oxygen_diagnostics = (0..bit_width).fold(diagnostics.clone(), |diagnostics, bit| {
+            let bit_counts: u32 = diagnostics.iter().map(|v| v.get(bit).unwrap()).sum();
+            let most_common_bit = if bit_counts >= diagnostics.len() as u32 - bit_counts {
+                1
+            } else {
+                0
+            };
+            diagnostics
                 .into_iter()
-                .enumerate()
-                .filter_map(|(index, value)| {
-                    if remove_list.contains(&index) {
-                        None
-                    } else {
-                        Some(value)
-                    }
-                })
-                .collect();
-            println!("kept {:?}", diagnostics);
-
-            // while i < diagnostics.len() {
-            //     if i in  {
-            //         let val = vec.remove(i);
-            //         // your code here
-            //     } else {
-            //         i += 1;
-            //     }
-        }
-        // for i in remove_list.iter() {
-        //     diagnostics.swap_remove(*i);
-        // }
-        println!("there are {} options left.", diagnostics.len());
-        let oxygen = diagnostics
-            .iter()
-            .next()
+                .filter(|p| *p.get(bit).unwrap() == most_common_bit)
+                .collect()
+        });
+        let oxygen: u32 = oxygen_diagnostics
+            .first()
             .unwrap()
-            // .chars()
             .iter()
             .rev()
-            // .rev()
-            // .map(|c| if c == '1' { 1 } else { 0 })
             .enumerate()
-            // .enumerate()
-            .fold(0, |a, (position, set)| a | set << position);
-        println!("oxygen: {}", oxygen);
-        let mut diagnostics: Vec<Vec<u32>> = input
-            .lines()
-            .map(|s| -> Vec<u32> {
-                s.chars()
-                    // .rev()
-                    .map(|c| if c == '1' { 1 } else { 0 })
-                    .collect()
-            })
-            .collect();
-        for bit in 0..12 {
+            .map(|(position, set)| set << position)
+            .sum();
+        let scrubber_diagnostics = (0..bit_width).fold(diagnostics, |diagnostics, bit| {
             if diagnostics.len() == 1 {
-                break;
-            }
-            println!("considering bit {}", bit);
-            let mut remove_list = Vec::new();
-            let bit_counts: (u32, u32) =
                 diagnostics
-                    .iter()
-                    .filter_map(|v| v.get(bit))
-                    .fold(
-                        (0, 0),
-                        |(t, f), i| {
-                            if *i == 1 {
-                                (t + 1, f)
-                            } else {
-                                (t, f + 1)
-                            }
-                        },
-                    );
-            println!("there are {} set and {} unset", bit_counts.0, bit_counts.1);
-            let least_common_bit = if bit_counts.0 >= bit_counts.1 { 0 } else { 1 };
-            println!("therefor the least common is {}", least_common_bit);
-            for (i, diagnostic) in diagnostics.iter().enumerate() {
-                if diagnostic[bit] != least_common_bit {
-                    remove_list.push(i);
-                    println!("will remove {}", i);
-                }
+            } else {
+                let popular_bit_count: u32 = diagnostics.iter().map(|v| v.get(bit).unwrap()).sum();
+                let unpopular_bit_count = diagnostics.len() as u32 - popular_bit_count;
+                let most_common_bit = if popular_bit_count >= unpopular_bit_count {
+                    1
+                } else {
+                    0
+                };
+                diagnostics
+                    .into_iter()
+                    .filter(|p| *p.get(bit).unwrap() != most_common_bit)
+                    .collect()
             }
-
-            diagnostics = diagnostics
-                .into_iter()
-                .enumerate()
-                .filter_map(|(index, value)| {
-                    if remove_list.contains(&index) {
-                        None
-                    } else {
-                        Some(value)
-                    }
-                })
-                .collect();
-            println!("kept {:?}", diagnostics);
-
-            // while i < diagnostics.len() {
-            //     if i in  {
-            //         let val = vec.remove(i);
-            //         // your code here
-            //     } else {
-            //         i += 1;
-            //     }
-        }
-        // for i in remove_list.iter() {
-        //     diagnostics.swap_remove(*i);
-        // }
-        println!("there are {} options left.", diagnostics.len());
-        let scrubber = diagnostics
-            .iter()
-            .next()
+        });
+        let scrubber: u32 = scrubber_diagnostics
+            .first()
             .unwrap()
-            // .chars()
             .iter()
             .rev()
-            // .rev()
-            // .map(|c| if c == '1' { 1 } else { 0 })
             .enumerate()
-            // .enumerate()
-            .fold(0, |a, (position, set)| a | set << position);
-
-        println!("scrubber: {}", scrubber);
+            .map(|(position, set)| set << position)
+            .sum();
+        // .fold(0, |a, (position, set)| a | set << position);
         let partb = oxygen * scrubber;
         println!("partb: {}", partb);
     }
