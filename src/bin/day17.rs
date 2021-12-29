4b29a2d9f5ececd955c2f53e3e0c15a80b87833b
Daniel Boyle <boylede+github@gmail.com>
2021-12-29 15:45:09 -0500

    clean up day 17, remove visualization

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,11 +1,11 @@
-use std::{collections::HashMap, ops::Neg};
+use itertools::Itertools;
 
 const INPUT: &str = include_str!("../../input/day17.txt");
-const INPUT_TRICKY: &str = "target area: x=352..377, y=-49..-30"; // correct answer is 66, not 1176
-const TEST_INPUT_B: &str = "target area: x=819..820, y=-11..-5";
 const TEST_INPUT: &str = "target area: x=20..30, y=-10..-5";
-
 const TEST_INPUT_C: &str = "target area: x=119..176, y=-141..-84";
+const TEST_INPUT_B: &str = "target area: x=819..820, y=-11..-5";
+const INPUT_TRICKY: &str = "target area: x=352..377, y=-49..-30"; // correct answer is 66, not 1176
+
 fn main() {
     println!("day 17");
     let (a, b) = INPUT.split_once(", ").unwrap();
@@ -18,59 +18,26 @@ fn main() {
     let (_, min_y) = b.split_once("y=").unwrap();
     let min_y = min_y.parse::<i64>().unwrap();
 
-    // println!("target min x: {}", min_x);
-    // println!("target max x: {}", max_x);
-    // println!("target min y: {}", min_y);
-    // println!("target max y: {}", max_y);
-
-    // test(min_x, max_x, min_y, max_y, 7, 2);
-    // test(min_x, max_x, min_y, max_y, 6, 3);
-    // test(min_x, max_x, min_y, max_y, 9, 0);
-    // test(min_x, max_x, min_y, max_y, 17, -4);
-    // test(min_x, max_x, min_y, max_y, 6, 9);
     let minimum_x_velocity = (0..=min_x)
-            .filter(|x| (1..=(*x)).sum::<i64>() > min_x)
-            .min()
-            .unwrap();
+        .filter(|x| (1..=(*x)).sum::<i64>() > min_x)
+        .min()
+        .unwrap();
     let max_y_velocity = min_y * -1;
-    // println!("found mimimum x velocity of {}", minimum_x_velocity);
-    // println!("using max x velocity of {}", max_x);
-    // println!("using min y velocity {}", min_y);
-    // println!("using max y velocity {}", max_y_velocity);
-    let mut tests = Vec::new();
-    for vx in minimum_x_velocity..=max_x {
-        for vy in min_y..=max_y_velocity {
-            tests.push((vx, vy));
-        }
-    }
-    // println!("testing: {:?}", tests);
-    let solutions: Vec<_> = tests
-        .iter()
-        .rev()
+
+    let solutions: Vec<_> = (minimum_x_velocity..=max_x)
+        .cartesian_product(min_y..=max_y_velocity)
         .filter_map(|(vx, vy)| {
-            let s = test(min_x, max_x, min_y, max_y, *vx, *vy);
-            if let Some((y, map)) = s {
-                // println!("({},{}) yields {}", vx, vy, y);
-                Some((y, vx, vy, map))
+            let s = test(min_x, max_x, min_y, max_y, vx, vy);
+            if let Some(y) = s {
+                Some((y, vx, vy))
             } else {
                 None
             }
         })
         .collect();
 
-        
-        
-    // println!("reached {}, with ({},{})", highest_y, ivx, ivy);
-    // print_map(&map);
-
-    // let n = (min_y * -1) -1;
-    // let _best_height_hack = n*(n + 1)/2;
-    // println!("shortcut answer was {}", best_height_hack);
-
     {
-        let (highest_y, _ivx,_ivy,_map) = solutions.iter().max_by_key(|(y, _vx, _vy, _map)| *y)
-        .unwrap();
-
+        let (highest_y, _ivx, _ivy) = solutions.iter().max_by_key(|(y, _vx, _vy)| *y).unwrap();
         println!("parta: {}", highest_y);
     }
     {
@@ -79,48 +46,30 @@ fn main() {
     }
 }
 
-fn test(min_x: i64, max_x: i64, min_y: i64, max_y: i64, mut vx: i64, mut vy: i64) -> Option<(i64,HashMap<(i64,i64),char>)> {
-    let ovx = vx;
-    let ovy = vy;
-    // println!("testing ({},{})", vx, vy);
-    let mut map: HashMap<(i64, i64), char> = HashMap::new();
-    map.insert((0, 0), 'S');
+fn is_inside(x: i64, y: i64, min_x: i64, max_x: i64, min_y: i64, max_y: i64) -> bool {
+    x >= min_x && x <= max_x && y >= min_y && y <= max_y
+}
 
-    for y in min_y..=max_y {
-        for x in min_x..=max_x {
-            map.insert((x, y), 'T');
-        }
-    }
+fn test(min_x: i64, max_x: i64, min_y: i64, max_y: i64, mut vx: i64, mut vy: i64) -> Option<i64> {
+    let mut highest_y: i64 = 0;
     let mut x = 0;
     let mut y = 0;
-    // let mut vx = 17;
-    // let mut vy = -4;
-    // let mut hit = false;
-    while map.get(&(x, y)) != Some(&'T') {
-        // println!("marking {},{}",x,y);
-        map.entry((x, y)).or_insert('#');
+    while !is_inside(x, y, min_x, max_x, min_y, max_y) {
         let (nx, ny, nvx, nvy) = step(x, y, vx, vy);
         x = nx;
         y = ny;
         vx = nvx;
         vy = nvy;
+        if y > highest_y {
+            highest_y = y;
+        }
         if x > max_x || y < min_y {
             break;
         }
-        // if y > min_y && vy
-        // else if *hit_char == 'T' {
-        //     hit = true;
-        //     break;
-        // }
     }
-    let hit = map.get(&(x, y)) == Some(&'T');
-    map.insert((x, y), '#');
-    // print_map(&map);
-    let highest_y = map.keys().map(|(_, y)| *y).max().unwrap();
-    // println!("hit: {}, with highest y: {}", hit, highest_y);
+    let hit = is_inside(x, y, min_x, max_x, min_y, max_y);
     if hit {
-        // println!("test ({},{}) yielded {}", ovx, ovy, highest_y);
-        Some((highest_y, map))
+        Some(highest_y)
     } else {
         None
     }
@@ -132,36 +81,10 @@ fn step(mut x: i64, mut y: i64, mut vx: i64, mut vy: i64) -> (i64, i64, i64, i64
     match vx.signum() {
         0 => (),
         1 => vx -= 1,
-        -1 =>vx += 1,
-        _ => unreachable!()//unsafe { std::hint::unreachable_unchecked()}
+        -1 => vx += 1,
+        _ => unreachable!(),
     }
-    // if vx.is_positive() {
-    //     vx -= 1;
-    // } else {
-    //     if vx != 0 {
-    //         vx += 1;
-    //     }
-        
-    // }
 
     vy -= 1;
     (x, y, vx, vy)
-}
-
-fn print_map(map: &HashMap<(i64, i64), char>) {
-    let min_x = map.keys().map(|(x, _)| *x).min().unwrap();
-    let max_x = map.keys().map(|(x, _)| *x).max().unwrap();
-    let min_y = map.keys().map(|(_, y)| *y).min().unwrap();
-    let max_y = map.keys().map(|(_, y)| *y).max().unwrap();
-    for y in (min_y..=max_y).rev() {
-        print!("{:04}: ",y);
-        for x in min_x..=max_x {
-            if let Some(c) = map.get(&(x, y)) {
-                print!("{}", c);
-            } else {
-                print!(".");
-            }
-        }
-        println!("");
-    }
 }
